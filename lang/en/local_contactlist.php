e5040a61a174ee6dad0707eba789cd9f2d09fbd7
Angela Baier <angela.baier@univie.ac.at>
2020-05-12 11:14:15 +0200

    lang strings

--- lang/en/local_contactlist.php
+++ lang/en/local_contactlist.php
@@ -27,8 +27,8 @@
 $string['email'] = 'Email address';
 $string['errornotallowedonpage'] = 'You are not registerd in this course!';
 $string['globaldefault'] = 'default (instance-wide)';
-$string['gnly'] = 'Your contactlist visibility settings in this course (visible) are different from your instance-wide settings (not visible). You can change your instance-wide settings <a href="{$a->here}" > here </a>.';
-$string['gyln'] = 'Your contactlist visibility settings in this course (not visible) are different from your instance-wide settings (visible). You can change your instance-wide settings <a href="{$a->here}" > here </a>.';
+$string['gnly'] = 'Your contactlist visibility setting in this course (visible) is different from your instance-wide setting (not visible). You can change your instance-wide settings <a href="{$a->here}" > here </a>.';
+$string['gyln'] = 'Your contactlist visibility setting in this course (not visible) is different from your instance-wide setting (visible). You can change your instance-wide settings <a href="{$a->here}" > here </a>.';
 $string['gyly'] = 'You can change your instance-wide settings <a href="{$a->here}" > here </a>.';
 $string['invisible'] = 'invisible';
 $string['localvisibility'] = 'Course contact list visibility';
