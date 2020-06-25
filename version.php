d18c80651176fc101d5a16bf56abed1de7a44693
Angela Baier <angela.baier@univie.ac.at>
2020-06-25 09:58:50 +0200

    version readable

--- version.php
+++ version.php
@@ -30,3 +30,4 @@ $plugin->version   = 2020062500;        // The current plugin version (Date: YYY
 $plugin->requires  = 2019111200;        // Requires this Moodle version.
 $plugin->component = 'local_contactlist'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
+$plugin->release   = "v3.8.0";
