eaad2e123753cfe4c85ff71d9dbe66d351d5e7f9
Adrian Czermak <adrian.czermak@univie.ac.at>
2023-10-19 14:33:54 +0200

    version incr

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023050300;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023101900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;        // Requires this Moodle version.
 $plugin->component = 'local_contactlist'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release   = "v4.2.0";
+$plugin->release   = "v4.3.0";
