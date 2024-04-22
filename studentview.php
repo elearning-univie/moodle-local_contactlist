0d700a63325a194c3dec5760bc0fab1fd0b46b12
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2024-04-22 15:25:19 +0200

    fix 4.4 deletion of dataformatlib.php

--- studentview.php
+++ studentview.php
@@ -23,7 +23,6 @@
  */
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
-require_once($CFG->libdir.'/dataformatlib.php');
 require_once($CFG->dirroot . '/local/contactlist/locallib.php');
 require_once($CFG->dirroot . '/local/contactlist/contactlist_table.php');
 
