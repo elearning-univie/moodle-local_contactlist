b0c8bc115d243248bf17dc610e4e9f96ea4ee001
Karri Pajarinen <pajarinenk66@univie.ac.at>
2026-05-11 18:04:10 +0000

    Edit version.php

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2026050700;
-$plugin->requires  = 2024100700;
+$plugin->requires  = 2025100600;
 $plugin->component = 'local_contactlist';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release   = "v5.1-r2";
