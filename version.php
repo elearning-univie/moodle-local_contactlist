e7aa1c241252ffa3835aa4a738b570b8b9a7409c
Karri Pajarinen <pajarinenk66@univie.ac.at>
2026-05-18 17:27:03 +0200

    Fix renamed category bug

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026050700;
+$plugin->version   = 2026050701;
 $plugin->requires  = 2025100600;
 $plugin->component = 'local_contactlist';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release   = "v5.1-r2";
+$plugin->release   = "v5.1-r3";
